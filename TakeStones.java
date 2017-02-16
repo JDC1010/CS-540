aee83e8f31d1247be130cd4820c41e403bd97c73
JDC1010 <100134josh@gmail.com>
2017-02-16 16:10:43 -0600

    Add files via upload

---
+++ TakeStones.java
@@ -0,0 +1,172 @@
+/***************************************************************************************
+  CS540 - Section 2
+  Homework Assignment 2: Game Playing
+
+  TakeStones.java
+  This is the main class that implements functions for Take Stones playing!
+  ---------
+  	*Free to modify anything in this file, except the class name 
+  	You are required:
+  		- To keep the class name as TakeStones for testing
+  		- Not to import any external libraries
+  		- Not to include any packages 
+	*Notice: To use this file, you should implement 4 methods below.
+
+	@author: TA 
+	@date: Feb 2017
+*****************************************************************************************/
+
+import java.util.ArrayList;
+
+
+public class TakeStones {
+
+	final int WIN_SCORE = 100;	// score of max winning game
+	final int LOSE_SCORE = -100;// score of max losing game
+	final int INFINITY = 1000;	// infinity constant
+
+	/** 
+    * Class constructor.
+    */
+	public TakeStones () {};
+
+
+	/**
+	* This method is used to generate a list of successors 
+	* @param state This is the current game state
+	* @return ArrayList<Integer> This is the list of state's successors
+	*/
+	public ArrayList<Integer> generate_successors(GameState state) {
+		int lastMove = state.get_last_move();	// the last move
+		int size = state.get_size();			// game size
+		ArrayList<Integer> successors = new ArrayList<Integer>();	// list of successors
+
+		// TODO Add your code here
+
+		return successors;
+	}
+
+
+	/**
+	* This method is used to evaluate a game state based on 
+	* the given heuristic function 
+	* @param state This is the current game state
+	* @return int This is the static score of given state
+	*/
+	public int evaluate_state(GameState state) {
+		// if stone 1 is still available, score is 0
+		if (state.get_stone(1)) 
+			return 0;
+
+		int lastMove = state.get_last_move();
+		if (1 == lastMove) {
+			
+			// TODO Add your code here
+		
+		} else if (Helper.is_prime(lastMove)){
+
+			// TODO Add your code here
+
+		} else {
+
+			// TODO Add your code here
+		}
+	}
+
+
+	/**
+	* This method is used to get the best next move from the current state
+	* @param state This is the current game state
+	* @param depth Current depth of search
+	* @param maxPlayer True if player is Max Player; Otherwise, false
+	* @return int This is the number indicating chosen stone
+	*/
+	public int get_next_move(GameState state, int depth, boolean maxPlayer) {
+		int move = -1;			// the best next move 
+		int alpha = -INFINITY;	// initial value of alpha
+		int beta = INFINITY;	// initial value of alpha
+
+		// Getting successors of the given state 
+		ArrayList<Integer> successors = generate_successors(state);
+
+		// Check if depth is 0 or it is terminal state 
+		if (0 == depth || 0 == successors.size()) {
+			state.log();
+			Helper.log_alphabeta(alpha, beta);
+			return move;
+		}
+
+		// TODO Add your code here
+
+		// Print state and alpha, beta before return 
+		state.log();
+		Helper.log_alphabeta(alpha, beta);
+		return move;
+	}
+
+
+	/**
+	* This method is used to implement alpha-beta pruning for both 2 players
+	* @param state This is the current game state
+	* @param depth Current depth of search
+	* @param alpha Current Alpha value
+	* @param beta Current Beta value
+	* @param maxPlayer True if player is Max Player; Otherwise, false
+	* @return int This is the number indicating score of the best next move
+	*/
+	public int alphabeta(GameState state, int depth, int alpha, int beta, boolean maxPlayer) {
+		int v = INFINITY; // score of the best next move
+
+		// TODO Add your code here
+
+		// Print state and alpha, beta before return 
+		state.log();
+		Helper.log_alphabeta(alpha, beta);
+		return v;	
+	}
+
+
+	/**
+	* This is the main method which makes use of addNum method.
+	* @param args A sequence of integer numbers, including the number of stones,
+	* the number of taken stones, a list of taken stone and search depth
+	* @return Nothing.
+	* @exception IOException On input error.
+	* @see IOException
+	*/
+	public static void main (String[] args) {
+		try {
+			// Read input from command line
+			int n = Integer.parseInt(args[0]);		// the number of stones
+			int nTaken = Integer.parseInt(args[1]);	// the number of taken stones
+			
+			// Initialize the game state
+			GameState state = new GameState(n);		// game state
+			int stone;
+			for (int i = 0; i < nTaken; i++) {
+				stone = Integer.parseInt(args[i + 2]);
+				state.remove_stone(stone);
+			}
+
+			int depth = Integer.parseInt(args[nTaken + 2]);	// search depth
+			// Process for depth being 0
+			if (0 == depth)
+				depth = n + 1;
+
+			TakeStones player = new TakeStones();	// TakeStones Object
+			boolean maxPlayer = (0 == (nTaken % 2));// Detect current player
+
+			// Get next move
+			int move = player.get_next_move(state, depth, maxPlayer);	
+			// Remove the chosen stone out of the board
+			state.remove_stone(move);
+
+			// Print Solution 
+			System.out.println("NEXT MOVE");
+			state.log();
+
+		} catch (Exception e) {
+			System.out.println("Invalid input");
+		}
+	}
+}
